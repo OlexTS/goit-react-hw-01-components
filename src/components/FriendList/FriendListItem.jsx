e22d2bec64f54645885c66961128446aecbbb90a
OlexTS <sashokseven@gmail.com>
2022-12-30 19:10:08 +0200

    finished friend styles

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
+import { CiWifiOff, CiWifiOn } from "react-icons/ci"
 import css from './FriendListItem.module.css'
 
 export const FriendListItem = ({avatar, name, isOnline}) => {
     return <li className={css.item}>
-        {isOnline ? <span className={css.isOnline}></span> : <span className={css.isOffline}></span>} 
+        
+
+        {isOnline ? <span className={css.isOnline}><CiWifiOn/></span> : <span className={css.isOffline}><CiWifiOff/></span>} 
         <img className={css.avatar} src={avatar} alt={name} width="48" />
         <p className={css.name}>{name}</p>
 </li>
